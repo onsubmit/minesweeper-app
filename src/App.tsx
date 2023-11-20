6491a70ef28163c7dbd6dbe2cdd16baf6cc061fa
Andy <onsubmit@outlook.com>
2023-11-19 17:45:26 -0800

    Make game responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
-
+import styles from './App.module.css';
 import MineField from './components/MineField';
 import MineGrid from './mineGrid';
 function App() {
   return (
-    <>
+    <div className={styles.app}>
       <MineField mines={MineGrid.build(9, 9, 10)}></MineField>
-    </>
+    </div>
   );
 }
 
